51187ada072d0a878d8f2fb529d91a76a03f76cd
stratsync-rohit <rohit@stratsync.ai>
2025-08-12 12:26:17 +0530

    firebase config change

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,14 +1,11 @@
-
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDVyovmUsGl5u7ovIoJHSbq6TP-zWUCiKU",
-  authDomain: "stratsyncai.firebaseapp.com",
-  projectId: "stratsyncai",
+  authDomain: "instashare-b328c.firebaseapp.com",
+  projectId: "instashare-b328c",
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-
-
